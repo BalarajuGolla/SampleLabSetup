338b5adb8293977b1f2753a8f59dbae7d7a6be94
BalarajuGolla <43969489+BalarajuGolla@users.noreply.github.com>
2021-09-27 21:34:20 +0530

    updated variables

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "access_key" {
   default = "AKIAXETNQAOYJC5XDPFI"
-  }
+}
 variable "secret_key" {
   default = "KZDSlcy3Az/knZWqhNDohFAZBb3bPivn0hSwElh6"
-  }
+}
 variable "region" {
   default = "ap-south-1"
-  }
+}
