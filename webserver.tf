ccbfcb759fa654962f85ce8c68af96299cdbf503
BalarajuGolla <43969489+BalarajuGolla@users.noreply.github.com>
2021-09-27 22:12:36 +0530

    Update webserver.tf

--- webserver.tf
+++ webserver.tf
@@ -1,9 +1,8 @@
- resource "aws_instance" "sm-project-web-server"
+ resource "aws_instance" "sm-project-web-server" {
    ami               = "ami-0a23ccb2cdd9286bb"
    instance_type     = "t2.nano"
    key_name          = "demo"
-
-
+ }
    network_interface {
      device_index         = 0
      network_interface_id = aws_network_interface.web-server-nic.id
