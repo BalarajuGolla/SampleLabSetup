0c8975e4025019683fef10ff7acea317b0892089
BalarajuGolla <43969489+BalarajuGolla@users.noreply.github.com>
2021-09-28 15:51:58 +0530

    Update webserver.tf

--- webserver.tf
+++ webserver.tf
@@ -2,7 +2,6 @@
    ami               = "ami-0a23ccb2cdd9286bb"
    instance_type     = "t2.nano"
    key_name          = "demo"
- }
    network_interface {
      device_index         = 0
      network_interface_id = aws_network_interface.web-server-nic.id
