fce528431c312164548115eb2951a1080734d546
BalarajuGolla <43969489+BalarajuGolla@users.noreply.github.com>
2021-09-28 15:54:46 +0530

    Update webserver.tf

--- webserver.tf
+++ webserver.tf
@@ -1,4 +1,4 @@
- resource "aws_instance" "sm-project-web-server" {
+ resource "aws_instance""sm-project-web-server" {
    ami               = "ami-0a23ccb2cdd9286bb"
    instance_type     = "t2.nano"
    key_name          = "demo"
